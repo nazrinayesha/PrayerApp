7fe507ac067ef55b8c8738c1e41a4ba707ba34a1
nazrinayesha <168328634+nazrinayesha@users.noreply.github.com>
2024-05-10 10:31:45 +0530

    Update profile.page.ts

--- app/pages/tabs/profile/profile.page.ts
+++ app/pages/tabs/profile/profile.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonCardContent, IonContent,IonList,IonListHeader, IonItem, IonRadio,IonLabel,IonButton, IonHeader,IonRadioGroup,IonInput, IonTitle, IonToolbar, IonCheckbox } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCardContent, IonContent, IonList,IonListHeader, IonItem,IonRadio, IonCheckbox,IonButton, IonLabel,IonHeader, IonRadioGroup, IonInput, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ProfilePage implements OnInit {
 
