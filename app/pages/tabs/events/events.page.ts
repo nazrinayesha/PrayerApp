2204d28ab896416666ced8c10a3093c5fa9b85c7
nazrinayesha <168328634+nazrinayesha@users.noreply.github.com>
2024-05-10 10:15:46 +0530

    Update events.page.ts

--- app/pages/tabs/events/events.page.ts
+++ app/pages/tabs/events/events.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonDatetime, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.page.html',
   styleUrls: ['./events.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonDatetime,IonButton, CommonModule, FormsModule]
 })
 export class SparklePage implements OnInit {
 
