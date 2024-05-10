7b2bd699afeb96f66f15a76b7fc888578ee9f5be
nazrinayesha <168328634+nazrinayesha@users.noreply.github.com>
2024-05-10 10:35:23 +0530

    Update app.routes.ts

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePage } from  './pages/tabs/home/home.page';
+import { MorningPage } from './pages/tabs/morning/morning.page';
+import { EveningPage } from './pages/tabs/evening/evening.page';
+
 
 export const routes: Routes = [
   {
@@ -6,6 +10,9 @@ export const routes: Routes = [
     redirectTo: 'tabs',
     pathMatch: 'full',
   },
+  { path: 'home', component: HomePage },
+  { path: 'morning', component: MorningPage },
+  { path: 'evening', component: EveningPage },
   {
     path: 'tabs',
     loadChildren: () => import('./pages/tabs/tabs.routes').then((m) => m.routes),
