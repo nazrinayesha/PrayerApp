fa2e153f0845612007c044d189e02d85b5dceab2
nazrinayesha <168328634+nazrinayesha@users.noreply.github.com>
2024-05-10 10:23:03 +0530

    Update home.page.ts

--- app/pages/tabs/home/home.page.ts
+++ app/pages/tabs/home/home.page.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonButton, IonToolbar,IonRow, IonCol, IonGrid,IonLabel  } from '@ionic/angular/standalone';
 
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle,IonButton, IonToolbar,IonRow, IonCol, IonGrid,IonLabel, CommonModule, FormsModule]
 })
 export class HomePage implements OnInit {
+  features: any[] = [
+    {id: 1,  src: 'assets/sun 1.png', page: '' },
+    {id: 2,  src: 'assets/moon1.png', page: '' },
+  ];
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  navigateToMorningPage() {
+    this.router.navigateByUrl('/morning');
+  }
+  navigateToEveningPage() {
+    this.router.navigateByUrl('/evening');
+  }
 }
